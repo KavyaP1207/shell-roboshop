604904338418691a7fc69faf3103a608438c305b
kavya Poreddy <poreddykavya1207@gmail.com>
2026-02-22 08:06:06 +0530

    sessions

--- mysql.sh
+++ mysql.sh
@@ -27,25 +27,10 @@ VALIDATE(){ #functions receive inputs through args just like shell script args
     echo -e " $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
    fi 
 }
+dnf install mysql-server -y
+
+systemctl enable mysqld
+systemctl start mysqld  
 
 
-dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "disabling default redis"
-dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "enabling redis"
-dnf install redis -y &>>$LOG_FILE
-VALIDATE $? "installing redis" 
-
-sed  -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no'/etc/redis/redis.conf
-VALIDATE $? "allowing remote connection to redis"
-
-systemctl enable redis &>>$LOG_FILE
-VALIDATE $? "enabling redis"
-systemctl start redis &>>$LOG_FILE
-VALIDATE $? "starting redis"
-
-END_TIME=$(date +%s)
-
-TOTAL_TIME=$(($END_TIME - $START_TIME))
-
-echo -e "script executed in : $TOTAL_TIME seconds $N"
+mysql_secure_installation --set-root-pass RoboShop@1
