0dfe08e20bb8f877978b02d6983db61578f2c8d4
kavya Poreddy <poreddykavya1207@gmail.com>
2026-02-22 10:07:24 +0530

    sessions

--- shipping.sh
+++ shipping.sh
@@ -31,7 +31,8 @@ VALIDATE(){ #functions receive inputs through args just like shell script args
 }
 
 dnf install maven -y &>>$LOG_FILE
-
+ 
+id roboshop
 if [ $? -ne 0 ]; then 
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "system user "
@@ -55,6 +56,7 @@ unzip /tmp/shipping.zip &>>$LOG_FILE
 VALIDATE $? "unzip shipping "
 
 mvn clean package
+VALIDATE $? "cleaned all packages"
 mv target/shipping-1.0.jar shipping.jar 
 
 cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
